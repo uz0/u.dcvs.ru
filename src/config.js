97bffb4ccaf48cdebed3705990fd6d57285c89de
fenruga <fenruga@yandex-team.ru>
2018-08-21 23:35:34 +0300

    discord bot init

--- src/config.js
+++ src/config.js
@@ -1,11 +1,21 @@
+const PREFIX = '!';
+
 module.exports = {
-  mongoURI: process.env.MONGODB_URI,
-  mongoURIdb: process.env.MONGODB_URI_DB,
-  url: process.env.APP_URL || 'https://bot-hyperloot.herokuapp.com:443',
-  telegram: {
-    credentials: {
-      authToken: process.env.TELEGRAM_AUTH_TOKEN,
+    mongoURI: process.env.MONGODB_URI,
+    mongoURIdb: process.env.MONGODB_URI_DB,
+    url: process.env.APP_URL || 'https://bot-hyperloot.herokuapp.com:443',
+    discord: {
+        credentials: {
+            authToken: process.env.DISCORD_AUTH_TOKEN || 'NDc5Njg4MTcwODM3OTAxMzcx.Dlyd6Q.dCvaAquznBiAQAaZSQXQFyAx9k8',
+        },
+        webhookEndpoint: process.env.DISCORD_WEBHOOKENDPOINT,
+        prefix: PREFIX,
     },
-    webhookEndpoint: process.env.TELEGRAM_WEBHOOKENDPOINT,
-  },
-}
+    telegram: {
+        credentials: {
+          authToken: process.env.TELEGRAM_AUTH_TOKEN || '639983676:AAHMdWPHL_s81ElXEvC6-OWq0jpHUoEtW-Q',
+        },
+        webhookEndpoint: process.env.TELEGRAM_WEBHOOKENDPOINT,
+        prefix: PREFIX,
+    },
+};
