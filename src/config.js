30e7b41969f8aa888b4a4bc8cef661449241aded
fenruga <fenruga@yandex-team.ru>
2018-08-23 00:39:00 +0300

    config upd

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ module.exports = {
   url: process.env.APP_URL || 'https://bot-hyperloot.herokuapp.com:443',
   telegram: {
     credentials: {
-      authToken: process.env.TELEGRAM_AUTH_TOKEN,
+      authToken: process.env.TELEGRAM_AUTH_TOKEN || '639983676:AAHMdWPHL_s81ElXEvC6-OWq0jpHUoEtW-Q',
     },
     webhookEndpoint: process.env.TELEGRAM_WEBHOOKENDPOINT,
   },
-}
+};
