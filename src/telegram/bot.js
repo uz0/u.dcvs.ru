a94c12b5203ea5087bba0bbbe7f2b2026d6f00d8
fenruga <fenruga@yandex-team.ru>
2018-08-29 23:33:40 +0300

    trying out queues

--- src/telegram/bot.js
+++ src/telegram/bot.js
@@ -13,24 +13,82 @@ const path = `/telegram/${config.telegram.webhookEndpoint}${telegramCfg.credenti
 const bot = new TelegramBot(telegramCfg.credentials.authToken, {polling: true});
 bot.setWebHook(`${config.url}${path}`);
 
-bot.on('message', (msg) => {
-    if (!_.startsWith(msg.text, PREFIX)) {
-        bot.sendMessage(msg.chat.id, `Enter command, pls (${PREFIX})`);;
-    }
+const HELP_MESSAGE =
+    `${PREFIX}mission to get a new task or get info about your current task;`;
 
+const MISSIONS = [
+    {
+        gamer: [{
+            name: 'gamer mission 1',
+            steps: []
+        }]
+    },
+    {
+        programmer: [{
+            name: 'programmer mission 1',
+            steps: []
+        }]
+    },
+];
+
+bot.on('message', (msg) => {
     const cmd = _.trimStart(msg.text, PREFIX);
     const userId = msg.from.id;
     let answer;
 
+    if (!_.startsWith(msg.text, PREFIX)) {
+        users.find({telegramId: userId}, (err, user) => {
+            if (_.isEmpty(user)) {
+                answer = `Please, type ${PREFIX}hiper to sign in before get a mission`;
+            } else if (user.pending === 'missionChoice') {
+                // todo pick from available
+                const choise = _.parseInt(msg.text) - 1;
+
+                users.update(
+                    {telegramId: userId},
+                    {$set: {onMission: true, currentMission: MISSIONS[choise][0].name, missionStep: 0}}
+                );
+
+                answer = `Mission picked ${MISSIONS[choise][0].name}!\n`;
+            }
+            bot.sendMessage(msg.chat.id, answer);
+        });
+
+        return;
+    }
+
+    // todo move out
+    if ('help' === cmd) {
+        bot.sendMessage(msg.chat.id, HELP_MESSAGE);
+    }
     if ('hiper' === cmd) {
         users.find({telegramId: userId}, (err, res) => {
             if (!_.isEmpty(res)) {
                 answer = 'Already signed!';
             }
             else {
-                users.insert({telegramId: userId}, );
+                users.insert({telegramId: userId}, {available: {gamer: 1, programmer: 1}});
                 answer = 'Successfully added!';
             }
+            bot.sendMessage(msg.chat.id, answer);
+        });
+    }
+    if ('mission' === cmd) {
+        users.find({telegramId: userId}, (err, user) => {
+            if (_.isEmpty(user)) {
+                answer = `Please, type ${PREFIX}hiper to sign in before get a mission`;
+            }
+            if (!user.onMission) {
+                users.update(
+                    {telegramId: userId},
+                    {$set: {pending: 'missionChoice'}}
+                );
+                // todo pick from available
+                answer = `Please, choose you mission (type number):\n1. Gamer\n2. Programmer`
+            } else {
+                answer = `Your current mission is ${user.currentMission}`;
+            }
+
             bot.sendMessage(msg.chat.id, answer);
         });
     }
