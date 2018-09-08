aa355d545a325afaa4c24c03b9f0481e53ac1a3c
dcversus <dcversus@gmail.com>
2018-09-08 21:12:45 +0300

    list refactor

--- src/modules/list.command.js
+++ src/modules/list.command.js
@@ -6,25 +6,24 @@ function getMissionMessage(mission) {
 }
 
 module.exports = async function(response, { input, db, id }) {
-    return new Promise((resolve, reject) => {
-        const {user} = response;
-        //todo  move to init checks
-        if (!user) {
-            response.output = 'You\'re not logged in yet';
-        }
-        else {
-            if (_.isEmpty(user.available)) {
-                response.output = 'No available missions for now <____>';
-            }
-            else {
-                response.output =
-                    _.map(user.available, mission => getMissionMessage(mission))
-                    .join('\n');
-            }
-        }
+    const {user} = response;
 
-        resolve(response);
-    });
+    if (!user) {
+        response.output = 'You\'re not logged in yet';
+    }
+
+
+    if (_.isEmpty(user.available)) {
+        response.output = 'No available missions for now <____>';
+    }
+
+    if (!response.output) {
+        response.output =
+            _.map(user.available, mission => getMissionMessage(mission))
+            .join('\n');
+    }
+
+    return Promise.resolve(response);
 };
 
 module.exports.command = 'list';
