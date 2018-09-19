923a8e3cb0abf305cf31f502fbf6a8df2692c020
dcversus <dcversus@gmail.com>
2018-09-19 17:01:42 +0300

    empty string i18n

--- src/i18n.js
+++ src/i18n.js
@@ -11,7 +11,7 @@ function i18nFactory() {
     reader.read('i18n.csv', rawData)
 
     return (key, props = {}) => {
-        let string = rawData[key];
+        let string = rawData[key] || "";
 
         if (!string && !props.strict) {
             string = key;
