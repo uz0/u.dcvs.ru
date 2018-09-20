d683b98a748549b08e1f98667fd108e6481f0f7d
dcversus <dcversus@gmail.com>
2018-09-21 00:54:10 +0300

    hot syntax error fix1

--- src/modules/moderation/index.js
+++ src/modules/moderation/index.js
@@ -5,13 +5,8 @@ const moderatorCheck = require('./moderatorCheck');
 const isModerator = require('./moderator');
 const setmoderator = require('./setmoderator.command');
 const unsetmoderator = require('./unsetmoderator.command');
-<<<<<<< HEAD
 // const linkedInComplete = require('./linkedInComplete');
-const complete = require('./complete');
-=======
-const linkedInComplete = require('./linkedInComplete.command');
 const complete = require('./complete.command');
->>>>>>> ac29d11e3a7be779504fd927f8e0a749f12a8cbb
 
 module.exports = [
     isModerator,
