598e6eb877681b1457b1c1380b170b05ab455cf2
dcversus <dcversus@gmail.com>
2018-12-19 17:45:31 +0300

    hotfix command return response

--- src/modules/command.js
+++ src/modules/command.js
@@ -6,7 +6,7 @@ module.exports = rawCommand => (response, { input }) => {
     const [cmd, ...rawArgs] = input.split(' ');
 
     if (!cmd.startsWith(`${PREFIX}`)) {
-        return null;
+        return response;
     }
 
     if (cmd.substring(1) !== command) {
