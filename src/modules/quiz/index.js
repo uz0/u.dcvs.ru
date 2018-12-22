91a615ff4cb99a538b68824cca957fc6c4c220c5
dcversus <dcversus@gmail.com>
2018-12-22 17:24:49 +0300

    quiz check

--- src/modules/quiz/index.js
+++ src/modules/quiz/index.js
@@ -1,9 +1,21 @@
 const command = require('../command');
 
-function quiz(response) {
+function addQuiz(response, {}) {
     console.log('hello i am empty module');
 
     return response;
 }
 
-module.exports = [command('quiz description prize ...answers'), quiz];
+function checkQuiz(response) {
+
+    return response;
+}
+
+module.exports = [
+    [command('quiz description prize ...answers'), addQuiz],
+    checkQuiz,
+]
+
+// for testing propo sials
+module.exports.addQuiz = addQuiz;
+module.exports.checkQuiz = checkQuiz;
