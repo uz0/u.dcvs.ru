1c36f9811d817504ba4e392a2f13bec3f3d05451
Vasilisa Versus <dcversus@gmail.com>
2019-02-15 16:23:57 +0300

    Update index.jsx

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,4 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './main.css';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const app = document.getElementById("app");
+
+if (app) {
+    ReactDOM.render(<App />, app);
+}
