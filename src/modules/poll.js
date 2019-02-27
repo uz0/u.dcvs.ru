c990c44fcee29decd4558874347bb0e45ab7b496
Dmitriy <dim0k92sokol@gmail.com>
2019-02-27 15:32:03 +0200

    return unique code and some fixes

--- src/modules/poll.js
+++ src/modules/poll.js
@@ -1,5 +1,5 @@
-const command = require('./command.filter');
 const { hri } = require('human-readable-ids');
+const command = require('./command.filter');
 // ШАГ 1 КАРКАС
 // ШАГ 2 описание структуры \ требования
 // 1 создать голосование
@@ -16,7 +16,7 @@ const { hri } = require('human-readable-ids');
 
 
 // 3 посмотреть результат
-// вывести все голосования и 
+// вывести все голосования и
 // подсчитать количество голосов для каждого варианта
 
 // ШАГ 3 реализация базового функционала
@@ -27,7 +27,7 @@ const { hri } = require('human-readable-ids');
 
 // один юзер не может голосовать дважды!
 // привязать вариант ответа, к голосованию
-// ...
+// создать и вернуть ид голосования в чат при создании.
 // ...
 // ...
 // ...
@@ -35,14 +35,19 @@ const { hri } = require('human-readable-ids');
 
 // ШАГ N описать что хотелка делает (с шага 2 по шаг 4)
 
-const addPoll = async function (request, { i18n, send, updateModuleData, getModuleData }) {
+const addPoll = async function (request, {
+    i18n,
+    send,
+    updateModuleData,
+    getModuleData,
+}) {
     const { args: { description, options } } = request;
-
+    const pollId = hri.random();
     const newPoll = {
         description,
         options,
-        id: hri.random(),
-    }
+        pollId,
+    };
 
     const { pollList = [] } = await getModuleData('poll');
 
@@ -50,37 +55,41 @@ const addPoll = async function (request, { i18n, send, updateModuleData, getModu
         pollList: [
             ...pollList,
             newPoll,
-        ]
+        ],
     });
 
-    send(i18n('poll.created'));
+    send(i18n('poll.created', { pollId }));
 
     return request;
 };
 
-const votePoll = async function (request, { i18n, send, updateModuleData, getModuleData }) {
+const votePoll = async function (request, {
+    i18n,
+    send,
+    updateModuleData,
+    getModuleData,
+}) {
     const { args: { option }, userId } = request;
 
     const { voteList = [], pollList = [] } = await getModuleData('poll');
 
-    const poll = pollList.reverse().find(poll => {
-        return poll.options.includes(option);
-    });
+    const poll = pollList.reverse().find(pollOption => pollOption.options.includes(option));
 
     if (!poll) {
-        send(i18n('vote.error'));
+        send(i18n('vote.errorNotPoll'));
 
         return request;
     }
 
-    const newVote = { option, userId, pollId: poll.id };
+    const newVote = { option, userId, pollId: poll.pollId };
 
-    const prevVoted = voteList.find(vote => {
-        return vote.option === option && vote.userId === userId && vote.pollId === poll.id;
-    });
+    const prevVoted = voteList.find(
+        vote => vote.option === option && vote.userId === userId && vote.pollId === poll.pollId,
+    );
+    console.log(prevVoted);
 
     if (prevVoted) {
-        send(i18n('vote.error'));
+        send(i18n('vote.errorPrevVoted'));
 
         return request;
     }
@@ -89,7 +98,7 @@ const votePoll = async function (request, { i18n, send, updateModuleData, getMod
         voteList: [
             ...voteList,
             newVote,
-        ]
+        ],
     });
 
     send(i18n('vote.cast'));
@@ -98,14 +107,10 @@ const votePoll = async function (request, { i18n, send, updateModuleData, getMod
 };
 
 const polls = async function (request, { i18n, send, getModuleData }) {
-    const { args: { option }} = request;
-
-    const newVote = { option };
-
     const { pollList = [], voteList = [] } = await getModuleData('poll');
 
-    pollList.forEach(poll => {
-        let optionResults = poll.options.map(option => {
+    pollList.forEach((poll) => {
+        const optionResults = poll.options.map((option) => {
             const results = voteList.filter(vote => vote.option === option);
 
             return `${option}:${results.length}`;
@@ -113,9 +118,9 @@ const polls = async function (request, { i18n, send, getModuleData }) {
 
         send(i18n('poll.info', {
             description: poll.description,
-            results: optionResults.join(', ')
+            results: optionResults.join(', '),
         }));
-    })
+    });
 
     return request;
 };
@@ -123,5 +128,5 @@ const polls = async function (request, { i18n, send, getModuleData }) {
 module.exports = [
     [command('addPoll description ...options'), addPoll],
     [command('votePoll option'), votePoll],
-    [command('polls'), polls]
+    [command('polls'), polls],
 ];
