ef0e1a9bb791944fdfca06d6d51b696c3fe9af5a
Vasilisa Versus <dcversus@gmail.com>
2019-03-02 23:35:08 +0300

    Update selfReact.js

--- src/modules/selfReact.js
+++ src/modules/selfReact.js
@@ -1,8 +1,10 @@
 const { selfName } = require('../config');
 
 const selfReact = async function (request, { i18n, send }) {
+    const { input, userId: id } = request;
+    
     if (request.input.includes(selfName)) {
-        send(i18n('selfReact'));
+        send(i18n('selfReact', { input, id }));
     }
 
     return request;
