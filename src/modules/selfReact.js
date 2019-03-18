0fd36a801d7d6a6417635526b209c02b4800f425
Vasilisa Versus <dcversus@gmail.com>
2019-03-18 11:40:07 +0300

    dont send reaction twice

--- src/modules/selfReact.js
+++ src/modules/selfReact.js
@@ -3,12 +3,14 @@ const { selfName } = require('../config');
 const selfReact = async function (request, { i18n, send }) {
     const { input, userId: id } = request;
 
-    if (request.input.includes(selfName)) {
-        send(i18n('selfReact', { input, id }));
-    }
-
     if (request.input.includes('?') && request.input.includes(selfName)) {
         send(i18n('selfAnswer', { input, id }));
+
+        return request;
+    }
+
+    if (request.input.includes(selfName)) {
+        send(i18n('selfReact', { input, id }));
     }
 
     return request;
