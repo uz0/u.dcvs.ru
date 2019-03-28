062ad8c59dc9f5204d59fbb22c79fa20bfd76081
Vasilisa Versus <dcversus@gmail.com>
2019-03-28 11:59:01 +0300

    Update funnyBan.js

--- src/modules/funnyBan.js
+++ src/modules/funnyBan.js
@@ -1,7 +1,7 @@
 const random = require('lodash/random');
 
 const funnyBan = async function (request, { i18n, send }) {
-    if (random(0, 5) === 3) {
+    if (random(0, 50) === 24) {
         send(i18n('ban'));
     }
 
